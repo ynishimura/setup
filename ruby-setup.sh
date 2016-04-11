8d20e8c663dc4fabbb69174ea7137029f0491d31
vagrant <vagrant@localhost.localdomain>
2016-04-11 11:21:21 +0200

    setup rbenv

---
+++ ruby-setup.sh
@@ -0,0 +1,20 @@
+sudo yum -y install gcc zlib zlib-devel openssl openssl-devel
+sudo yum -y install epel-release
+sudo yum -y install gcc-c++ git glibc-headers libffi-devel libxml2 libxml2-devel libxslt libxslt-devel libyaml-devel make nodejs npm  readline readline-devel sqlite-devel
+
+##rbenv
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+## PATH
+
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+##check
+
+rbenv -v
